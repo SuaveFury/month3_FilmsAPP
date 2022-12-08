2ec52df20aacda00cc8fc9aefa7accb5a4740662
AhmadMajali <amajali392@gmail.com>
2022-12-08 11:04:55 +0600

    new

--- app/src/main/java/com/example/hw38/SecondFragment.kt
+++ app/src/main/java/com/example/hw38/SecondFragment.kt
@@ -11,7 +11,7 @@ import com.example.hw38.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private lateinit var binding: FragmentSecondBinding
-    private val filmList = arrayListOf<Film>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
